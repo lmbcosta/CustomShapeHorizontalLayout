664b06bc14af650f4fc8e4ec05fcae1b1032e62e
lmbcosta <lmbcosta@hotmail.com>
2018-10-21 20:10:39 +0100

    Initial Commit

---
+++ CustomShapeHorizontalLayout/CustomShapeHorizontalLayout.h
@@ -0,0 +1,19 @@
+//
+//  CustomShapeHorizontalLayout.h
+//  CustomShapeHorizontalLayout
+//
+//  Created by Luis  Costa on 21/10/2018.
+//  Copyright © 2018 Luis  Costa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomShapeHorizontalLayout.
+FOUNDATION_EXPORT double CustomShapeHorizontalLayoutVersionNumber;
+
+//! Project version string for CustomShapeHorizontalLayout.
+FOUNDATION_EXPORT const unsigned char CustomShapeHorizontalLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomShapeHorizontalLayout/PublicHeader.h>
+
+
